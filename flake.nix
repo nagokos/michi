cc222d37c75e6e70126768abb84b004750e32e98
naohiro kosuda <kosnago0428@gmail.com>
2024-10-31 03:04:34 +0900

    add flake nix

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      fenix,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            (fenix.packages.${system}.combine [
+              fenix.packages.${system}.stable.toolchain
+            ])
+          ];
+        };
+      }
+    );
+}
